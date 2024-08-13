e5266dd50d6a7aac936f3a080765819e7bc60861
rizkynat <rizkynathamael@gmail.com>
2024-08-13 17:26:05 +0700

    Fix route

--- __test__/page.test.tsx
+++ __test__/page.test.tsx
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import Main from '../src/app/main'
+import Page from '../src/app/page'
 import React from 'react'
  
 describe('Page', () => {
   it('should render the main element', () => {
-    render(<Main />)
+    render(<Page />)
  
     const main = screen.getByTestId("main")
  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Wrapper from "../components/wrapper";
 import React from "react";
-export default function Main() {
+export default function Page() {
 
   return (
     <main data-testid="main" className="flex min-h-screen flex-col items-center justify-center tablet:p-24 bg-background">
