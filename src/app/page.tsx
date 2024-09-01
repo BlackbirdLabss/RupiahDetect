95b7984eefce2c57e8f96bcf3433e45afe9b7e10
rizkynat <rizkynathamael@gmail.com>
2024-09-01 22:41:25 +0700

    Make integration with api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import Wrapper from "../components/wrapper";
+import UploadImage from "./upload-image";
 import React from "react";
 export default function Page() {
-
   return (
-    <main data-testid="main" className="flex min-h-screen flex-col items-center justify-center tablet:p-24 bg-background">
-        <Wrapper></Wrapper>
+    <main
+      data-testid="main"
+      className="flex min-h-screen flex-col items-center justify-center tablet:p-24 bg-background"
+    >
+      <UploadImage />
+      <footer className="flex justify-center items-end w-full h-24 p-3 absolute bottom-0">
+        created by{"\u00A0"}
+        <a
+          href="https://github.com/BlackbirdLabss"
+          target="_blank"
+        >
+          <b>
+            Blackbird<span className="text-[#2da1fc]">Labss</span>
+          </b>
+        </a>
+      </footer>
     </main>
   );
 }
---
+++ src/components/drag-and-drop.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import React from "react";
+
+const defaultImage = "/upload-icon.png";
+
+function DragAndDrop({
+  handleImageUpload,
+}: {
+  handleImageUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <Image
+          src={defaultImage}
+          alt="upload icon"
+          width="100"
+          height="59"
+        />
+
+        <div className="text-base font-bold mt-6">
+          <span className="text-[#0f0f0f]">Drag & drop files or </span>
+          <div className="relative inline-block">
+            <span className="text-primary underline relative">Browse</span>
+            <input
+              className="absolute top-0 left-0 opacity-0 w-full h-full"
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+            />
+          </div>
+        </div>
+
+        <div className="text-xs text-[#676767]">
+          Format berupa: JPEG, PNG, JPG
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DragAndDrop;
